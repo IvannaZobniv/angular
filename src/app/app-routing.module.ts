0f57bf52614ccce4be3a5abfce04034cfd9c94f8
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-06-08 12:25:20 +0300

    angular lesson5-part3 guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {MainLayoutsComponent} from "./layouts/main-layouts/main-layouts.component";
+import {isAuthGuard} from "./guards/is-auth.guard";
 
 const routes:Routes = [
   {path:'', component:MainLayoutsComponent, children:[
       {path:'', redirectTo:'auth/login', pathMatch:'full'},
       {path:'auth', loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)},
-      {path:'cars', loadChildren:()=>import('./modules/cars/cars.module').then(m=>m.CarsModule)}
+      {path:'cars',canActivate: [isAuthGuard], loadChildren:()=>import('./modules/cars/cars.module').then(m=>m.CarsModule)}
     ]}
 ]
 
